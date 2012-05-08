660a9818f479972ee2184298be1b545953b541de
laomoi <laomoi@qq.com>
2012-05-09 01:10:00 +0800

    get len

---
+++ c/utf8len.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int getUTF8len(char* str) {
+    return 0;
+}
+int main(void)
+{
+    char* a = "woriwori";
+    int l = getUTF8len(a);
+    
+    
+    printf("Hello, world. %d\n", l);
+    return 0;
+}
+
+
